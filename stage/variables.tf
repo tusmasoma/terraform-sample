cd97942b5c070f7b0a0a829b1adb461de9f5b2fd
tusmasoma <s.k18@icloud.com>
2024-05-04 18:11:19 +0900

    feat: add stage setttings

---
+++ stage/variables.tf
@@ -0,0 +1,35 @@
+variable "cidr_vpc" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overriden"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "cidr_public" {
+    description = "A list of public subnet CIDR blocks"
+    type        = list(string)
+    default     = []
+}
+
+variable "cidr_private" {
+    description = "A list of private subnet CIDR blocks"
+    type        = list(string)
+    default     = []
+}
+
+variable "cidr_secure" {
+    description = "A list of secure subnet CIDR blocks"
+    type        = list(string)
+    default     = []
+}
+
+variable "system" {
+    description = "The system to be deployed"
+    type        = string
+    default     = ""
+}
+
+variable "env" {
+    description = "The environment to be deployed"
+    type        = string
+    default     = ""
+}
